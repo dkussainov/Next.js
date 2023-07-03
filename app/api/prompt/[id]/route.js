fd8df0026b9a10356cf5c4fe23de5804d7337b8c
Dulat <dulat.kussainov@gmail.com>
2023-07-03 14:31:06 -0400

    nextjs project authentication and crud added

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,30 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDB();
+    const prompt = await Prompt.findById(params.id).populate("creator");
+
+    if (!prompt) return new Response("Prompt not found", { status: 404 });
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch prompt", { status: 500 });
+  }
+};
+
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+    connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt)
+      return new Response("Prompt not found", { status: 404 });
+  } catch {}
+};
+
+export const DELETE = async (request, { params }) => {};
