68389edea0018c9e81e0c906f76313b1d404323a
Dulat <dulat.kussainov@gmail.com>
2023-07-12 16:07:46 -0400

    CRUD added

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -18,16 +18,32 @@ export const PATCH = async (request, { params }) => {
   const { prompt, tag } = await request.json();
 
   try {
-    connectToDB();
+    await connectToDB();
 
     const existingPrompt = await Prompt.findById(params.id);
 
     if (!existingPrompt)
       return new Response("Prompt not found", { status: 404 });
 
-    //stopped at 2:51
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
 
-  } catch {}
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update prompt", { status: 500 });
+  }
 };
 
-export const DELETE = async (request, { params }) => {};
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
+
+    await Prompt.findByIdAndRemove(params.id);
+
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    return new Response("Failed to delete prompt", { status: 500 });
+  }
+};
